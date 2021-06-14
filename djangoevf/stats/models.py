5df4338ced95f1a109cd48bcd6909a1d43823d18
Marcelino Rodriguez <mrg0809@gmail.com>
2021-06-14 00:08:36 -0500

    menu login, modelo perfil, bulma navbar

--- djangoevf/stats/models.py
+++ djangoevf/stats/models.py
@@ -1,6 +1,15 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Perfil(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    email = models.EmailField
+    tipo = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.user
+
 class Almacen(models.Model):
     numero = models.CharField(max_length=8)
     nombre = models.CharField(max_length=55)
