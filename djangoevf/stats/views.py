62005406c70a166d561fc49bc39fbbb30fa91847
Marcelino Rodriguez <mrg0809@gmail.com>
2021-07-27 20:40:25 -0500

    Se completan consultas para el dashboard y se acomoda tabla de ventas

--- djangoevf/stats/views.py
+++ djangoevf/stats/views.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
+from os import replace
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -13,7 +14,7 @@ from .serializer import AlmacenSerializer
 """Datos para conexion a base de datos remota"""
 conn = firebirdsql.connect(
     host='sfan.ddns.net',
-    database='C:\Microsip datos\PRUEBAS.FDB',
+    database='C:\Microsip datos\SPORTS FAN 2016.FDB',
     port=2000,
     user='SYSDBA',
     password='flexracer',
@@ -61,17 +62,84 @@ def ventas(request):
     par = [fechabuena, fechabuena, 0, 'N', 0, 'a', 0, 'S']
     cur.execute("select * from VENTA_DESGL_PER(?, ?, ?, ?, ?, ?, ?, ?)", (par))
     ventas = cur.fetchall()
+    print(ventas)
+    total = 0
+    for x in ventas:
+        total = total+x[4]
+        
     datos = {
         "ventas":ventas,
-        "fecha":fechabuena
+        "fecha":fechabuena,
+        "total":total
     }           
     return render(request, "stats/ventas.html", datos)
 
 
 @login_required
 def dash(request):
+    """VARIABLES DE FECHAS"""
     now = datetime.now()
-    return render(request, "stats/dash.html", {"now" : now})
+    fecha = datetime.today().strftime('%d.%m.%Y')
+    ayer = (datetime.today() - timedelta(days=1)).strftime('%d.%m.%Y')
+    primerdia = datetime.today().replace(day=1).strftime('%d.%m.%Y')
+    primerdiaaa = (datetime.today() - timedelta(days=365)).replace(day=1).strftime('%d.%m.%Y')
+    primerdiaproxmes = (datetime.today().replace(day=28) + timedelta(days=4))
+    ultimodiaaa = (primerdiaproxmes - timedelta(days=primerdiaproxmes.day)).replace(year=2020)
+
+    
+    """OBTENER VENTA AL MOMENTO"""
+    par = [fecha, fecha, 0, 'N']
+    cur.execute("select * from VENTA_TOTAL_PER_PV(?, ?, ?, ?)", (par))
+    venta = cur.fetchone()
+
+    """OBTENER VENTA POR TIENDAS DE AYER"""
+    parayer = [ayer, ayer, 0, 'N', 0, 'a', 0, 'S']
+    cur.execute("select * from VENTA_DESGL_PER(?, ?, ?, ?, ?, ?, ?, ?)",(parayer))
+    ventas = cur.fetchall()
+    total = 0
+    for x in ventas:
+        total = total+x[4]
+
+    """OBTENER TRANSACCIONES POR TIENDAS DE HOY"""
+    partrans = [fecha, fecha, 0, 'N', 0, 'a', 0, 'S']
+    cur.execute("select * from VENTA_DESGL_PER(?, ?, ?, ?, ?, ?, ?, ?)",(partrans))
+    transacciones = cur.fetchall()
+    totaltransacciones = 0
+    for y in transacciones:
+        totaltransacciones = totaltransacciones+y[3]
+    totaltransacciones = int(totaltransacciones)
+
+    """TICKET PROMEDIO"""
+    ticket = format(venta[0]/totaltransacciones, '.2f')
+
+    """VENTA EN MES"""
+    parmesactual = [primerdia, fecha, 0, 'N']
+    cur.execute("select * from VENTA_TOTAL_PER_PV(?, ?, ?, ?)", (parmesactual))
+    ventames = cur.fetchone()
+
+    """VENTA MES AA"""
+    parmesaa = [primerdiaaa, ultimodiaaa, 0, 'N']
+    cur.execute("select * from VENTA_TOTAL_PER_PV(?, ?, ?, ?)", (parmesaa))
+    ventamesaa = cur.fetchone()
+
+    """DIFERENCIAS DE VENTA"""
+    diferencia = ventames[0] - ventamesaa[0]
+    diferenciap = format(ventames[0] / ventamesaa[0], '.1f')
+
+    datos = {
+        "venta":venta,
+        "fecha":fecha,
+        "now":now,
+        "ventas":ventas,
+        "total":total,
+        "totaltransacciones":totaltransacciones,
+        "ticket":ticket,
+        "ventames":ventames,
+        "ventamesaa":ventamesaa,
+        "diferencia":diferencia,
+        "diferenciap":diferenciap,
+    }
+    return render(request, "stats/dash.html", datos)
 
 def salir(request):
     logout(request)
