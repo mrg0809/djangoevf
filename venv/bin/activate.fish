e9dafcccb974a7874f2fb72b3b4967421de29f1f
Marcelino Rodriguez <mrg0809@gmail.com>
2022-06-07 17:17:14 -0500

    se corrige venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/rm/Desarrollo/djangoevf/venv"
+set -gx VIRTUAL_ENV "/home/marcelo/Desarrollo/djangoevf/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
